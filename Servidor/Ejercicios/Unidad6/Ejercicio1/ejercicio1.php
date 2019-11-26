ce8c38284045074436b38deafcd29dea0a397f09
asd <alejandro@gmail.com>
2019-11-26 20:55:15 +0100

    Bootstrap

---
+++ Servidor/Ejercicios/Unidad6/Ejercicio1/ejercicio1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Document</title>
+        <style>
+
+            div{
+                border: 3px solid black;
+                font-size: 100px;
+            }
+        </style>
+    </head>
+    <body>
+        <div>
+            Este sitio web utiliza cookies
+        </div>
+    </body>
+</html>
