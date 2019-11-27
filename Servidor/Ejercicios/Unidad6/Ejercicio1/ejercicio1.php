f26bc327a69d39bd55df85ff81eeba88640ce352
asd <alejandro@gmail.com>
2019-11-27 20:53:13 +0100

    Ejercicio1 php tema 6

--- Servidor/Ejercicios/Unidad6/Ejercicio1/ejercicio1.php
+++ Servidor/Ejercicios/Unidad6/Ejercicio1/ejercicio1.php
@@ -1,3 +1,14 @@
+<?php
+
+
+if(isset($_POST["cookie"])){
+    //Pongo 10 s para comprobar antes si funciona.
+    setcookie('cookie', 'aceptar', time()+10);
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="es">
     <head>
@@ -9,11 +20,20 @@
                 border: 3px solid black;
                 font-size: 100px;
             }
+            input{
+                width: 100px;
+                height: 50px;
+                margin: 1%;
+            }
         </style>
     </head>
     <body>
+       <?php if(!isset($_COOKIE["cookie"]) && !isset($_GET["cookie"])): ?>
         <div>
-            Este sitio web utiliza cookies
+            <form action="#" method="post"><label for="cookie">Este sitio web utiliza cookies</label><input type="submit" name="cookie" value="Aceptar"></form>
         </div>
+        <?php else: ?>
+        <p>Cookie Aceptada</p>
+        <?php endif ?>
     </body>
 </html>
