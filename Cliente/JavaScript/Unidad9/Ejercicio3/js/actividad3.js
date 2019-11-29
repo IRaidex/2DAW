afbeb10e0d676c0a441c84c70c33b2d618429cad
asd <alejandro@gmail.com>
2019-11-29 18:10:29 +0100

    Ejercicio 3 JAVA AJAX

--- Cliente/JavaScript/Unidad9/Ejercicio3/js/actividad3.js
+++ Cliente/JavaScript/Unidad9/Ejercicio3/js/actividad3.js
@@ -9,32 +9,18 @@ document.addEventListener("DOMContentLoaded", funCargar);
 function funCargar(){
 
     var httpRequest = obtainXMLHttpRequest();
-
-    httpRequest.open("POST", "https://breakingbadapi.com/api/characters", true);
-    httpRequest.onreadystatechange= function() {
-        if (httpRequest.readyState==2) {
-            document.getElementById("contenido").innerHTML = 'CARGANDO....<img sr="img/cargando.gif">';
-
-        }
-
-        if (httpRequest.readyState==4) {// Si se ha completado
+    
+    var arrayPersonajes = peticion(10,0);
+    
+    var div = document.getElementById("personajes");
+    div.innerHTML = '<div class="col-3"><div class="card bg-light my-2 mx-1 border border-dark"><img src="'+arrayPersonajes.img+'"><div class="card-body"><h5 class="card-title">'+arrayPersonajes.name+'</h5><p class"card-text">Apodo: '+arrayPersonajes.nickname+'<br>Fecha Nacimiento: '+arrayPersonajes.birthday+'</p><p class="card-text"><small class="text-muted">Interpretado por: '+arrayPersonajes.portrayed+'</small></p></div></div></div>';
 
 
-            // Se comprueba el estado de la petición
-            if (httpRequest.status==200){//  el código 200 indica que la petición se ha respondido correctamente
-
-
-                document.getElementById("contenido").innerHTML = httpRequest.responseText;
-            }
-        }
-    }
-
-    httpRequest.send("?limit=5&offset=45");
 
 }
 
 
-
+//Funcion para crear el httpRequest
 function obtainXMLHttpRequest(){
     var httpRequest;
     if (window.XMLHttpRequest)//El explorador implementa la interfaz de forma nativa
@@ -52,5 +38,35 @@ function obtainXMLHttpRequest(){
 }
 
 
+//Funcion para hacer la peticion y le pasamos el limit y offset
+function peticion(limite,offset){
+
+    var httpRequest = obtainXMLHttpRequest();
+
+    httpRequest.open("GET", 'https://breakingbadapi.com/api/characters?limit='+limite+'&offset='+offset, true);
+    httpRequest.onreadystatechange= function() {
+        if (httpRequest.readyState==2) {
+            document.getElementById("personajes").innerHTML = 'CARGANDO....<img sr="img/cargando.gif">';
+
+        }
+
+        if (httpRequest.readyState==4) {// Si se ha completado
+
+
+            // Se comprueba el estado de la petición
+            if (httpRequest.status==200){//  el código 200 indica que la petición se ha respondido correctamente
+
+                var objeto = JSON.parse(httpRequest.responseText);
+                return objeto;
+
+
+            }
+        }
+    }
+
+    httpRequest.send();
+
+
+}
 
 
