33308a607b7587abd1c43b3b9304627c43bb9db4
asd <alejandro@gmail.com>
2019-11-29 20:58:39 +0100

    SERVIDOR

---
+++ Servidor/Ejercicios/Unidad6/Ejercicio2/ejercicio2.php
@@ -0,0 +1,17 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Document</title>
+    </head>
+    <body>
+        <form action="#" method="post">
+            <input type="submit" name="dark" value="BLACK">
+            <input type="submit" name="light" value="LIGHT">
+        </form>
+    </body>
+</html>
