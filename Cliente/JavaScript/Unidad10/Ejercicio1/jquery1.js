eccd21d2b30c0c5908c60e3ce5382e9eccb081b0
asd <alejandro@gmail.com>
2019-12-09 18:05:32 +0100

    JQUERy1

---
+++ Cliente/JavaScript/Unidad10/Ejercicio1/jquery1.js
@@ -0,0 +1,3 @@
+$(function() {
+    alert("Este es mi primer documento con un script jQuery");              
+});
---
+++ Cliente/JavaScript/Unidad10/Ejercicio1/jquery2.js
@@ -0,0 +1,4 @@
+var JQ = jQuery;
+JQ(function() {
+    alert("Este es mi primer documento con un script jQuery2");              
+});
