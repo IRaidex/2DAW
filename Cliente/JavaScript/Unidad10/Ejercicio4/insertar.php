ed04aae4dca04667c15e86c5f75d29a1bb149c44
Alex <alejandrosal1396@gmail.com>
2019-12-15 19:10:00 +0100

    Terminado AC4,5 js

---
+++ Cliente/JavaScript/Unidad10/Ejercicio4/insertar.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once"conexion.php";
+
+var_dump($_POST);
+if($_POST["nombre"] != "" && $_POST["mail"] != "" && $_POST["comentario"] != ""){
+   
+    $consulta = $conexion->prepare('INSERT INTO comentarios (nombre,mail,comentario) VALUES (?,?,?);');
+    $consulta->bindParam(1, $_POST["nombre"]);
+    $consulta->bindParam(2, $_POST["mail"]);
+    $consulta->bindParam(3, $_POST["comentario"]);
+    $consulta->execute();
+    
+}
+
