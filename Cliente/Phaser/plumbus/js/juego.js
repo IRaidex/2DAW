f1e9dfcd5dfc3248363d252dee034c18657cf37b
asd <alejandro@gmail.com>
2020-01-10 18:07:03 +0100

    Juegov2

--- Cliente/Phaser/plumbus/js/juego.js
+++ Cliente/Phaser/plumbus/js/juego.js
@@ -49,14 +49,66 @@ function create ()
 {
     this.add.image(400,300 ,'fondo');
     plataforms = this.physics.add.staticGroup();
-    
+
     plataforms.create(400, 575, 'plataforma').setScale(1.1).refreshBody();
-    
+
     plataforms.create(600,400, 'plataforma').setScale(0.5).refreshBody();
     plataforms.create(50,250, 'plataforma').setScale(0.5).refreshBody();
     plataforms.create(750,220, 'plataforma').setScale(0.5).refreshBody();
+
+    player = this.physics.add.sprite(100, 450, 'morty');
+
+    player.setBounce(0);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: 'derecha',
+        frames: this.anims.generateFrameNumbers('morty', {start: 5, end: 8}),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'izquierda',
+        frames: this.anims.generateFrameNumbers('morty', {start: 3, end: 0}),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'parado',
+        frames: [ { key: 'morty', frame: 4} ],
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.physics.add.collider(player, plataforms);
+
+    cursors= this.input.keyboard.createCursorKeys();
+
 }
 
 function update ()
 {
+    if (cursors.left.isDown){
+
+        player.anims.play('izquierda', true);
+        player.setVelocityX(-100);
+
+    }else if(cursors.right.isDown){
+
+        player.anims.play('derecha', true);
+        player.setVelocityX(100);
+
+    }else{
+
+        player.anims.play('parado', false);
+        player.setVelocityX(0);
+
+    }
+
+    if (cursors.up.isDown && player.body.touching.down){
+        player.setVelocityY(-350);
+    }
+
 }
