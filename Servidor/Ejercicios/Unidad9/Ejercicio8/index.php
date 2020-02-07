b5c20b3a032c40babfe5b8f11f525614a374ccc9
Alex <alejandrosal1396@gmail.com>
2020-02-07 20:58:54 +0100

    PHP TERMINADO

--- Servidor/Ejercicios/Unidad9/Ejercicio8/index.php
+++ Servidor/Ejercicios/Unidad9/Ejercicio8/index.php
@@ -2,11 +2,12 @@
 if(isset($_GET['enviar'])){
 
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'https://localhost.servidor/consulta.php');
+    curl_setopt($ch, CURLOPT_URL, 'http://localhost.servidor/consulta.php/?tipo='.$_GET['action']);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $res = curl_exec($ch);
     curl_close($ch);
     $res = json_decode($res);
+
 }
 
 ?>
@@ -25,13 +26,14 @@ if(isset($_GET['enviar'])){
                 <option value="grupos">Grupos</option>
                 <option value="canciones">Canciones</option>
             </select>
-            <input type="submit" name="envio">
+            <input type="submit" name="enviar">
         </form>
         <?php if(isset($res)): ?>
-        <?php foreach($res->results as $valor): ?>
-        <p>Nombre: <?= $valor->name ?></p> 
-        <img src="<?= $valor->image ?>" alt="personajes">
-        <?php endforeach ?>
+            <?php foreach($res as $valor => $valores): ?>
+                <?php foreach($valores as $resultado => $alex): ?>
+                    <span><?= $resultado ?> : <?= $alex ?> - </span>
+                <?php endforeach ?><br>
+            <?php endforeach ?>
         <?php endif ?>
     </body>
 </html>
